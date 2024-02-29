2cc83f37af1de43b920b8327c4d4d63b07758cdd
DM <dmoosocool@gmail.com>
2024-02-29 13:35:01 +0800

    chore: support katex to script type math/tex

--- src/index.js
+++ src/index.js
@@ -1,24 +1,73 @@
-export default function(options = {}) {
-  // extension code here
+const inlineRule = /^(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$)/;
+const blockRule = /^(\${1,2})\n((?:\\[^]|[^\\])+?)\n\1(?:\n|$)/;
 
+export default function (options = {}) {
   return {
-    tokenizer: {
-      paragraph(src) {
-        if (src !== 'example markdown') {
-          return false;
+    extensions: [
+      inlineKatex(options, createRenderer(options, false)),
+      blockKatex(options, createRenderer(options, true))
+    ]
+  };
+}
+
+function createRenderer(options, newlineAfter) {
+  return (token) => {
+    return `<script type="math/tex${newlineAfter ? '; mode=display' : ''}">${token.text}</script>`;
+  };
+}
+
+function inlineKatex(options, renderer) {
+  return {
+    name: 'inlineKatex',
+    level: 'inline',
+    start(src) {
+      let index;
+      let indexSrc = src;
+
+      while (indexSrc) {
+        index = indexSrc.indexOf('$');
+        if (index === -1) {
+          return;
         }
-
-        const token = {
-          type: 'paragraph',
-          raw: src,
-          text: 'example html',
-          tokens: []
-        };
-
-        this.lexer.inline(token.text, token.tokens);
-
-        return token;
+        if (index === 0 || indexSrc.charAt(index - 1) === ' ') {
+          const possibleKatex = indexSrc.substring(index);
+          if (possibleKatex.match(inlineRule)) {
+            return index;
+          }
+        }
+        indexSrc = indexSrc.substring(index + 1).replace(/^\$+/, '');
       }
-    }
+    },
+    tokenizer(src, tokens) {
+      const match = src.match(inlineRule);
+      if (match) {
+        return {
+          type: 'inlineKatex',
+          raw: match[0],
+          text: match[2].trim(),
+          displayMode: match[1].length === 2
+        };
+      }
+    },
+    renderer
+  };
+}
+
+function blockKatex(options, renderer) {
+  return {
+    name: 'blockKatex',
+    level: 'block',
+    tokenizer(src, tokens) {
+      const match = src.match(blockRule);
+      if (match) {
+        return {
+          type: 'blockKatex',
+          raw: match[0],
+          text: match[2].trim(),
+          displayMode: match[1].length === 2
+        };
+      }
+    },
+    renderer
   };
 }
